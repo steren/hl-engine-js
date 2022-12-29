63a92516c72c733919a427c155121720b37138ae
Steren <steren.giannini@gmail.com>
2022-12-29 15:06:16 +0000

    remove unecessary memory init code

--- hl-engine.js
+++ hl-engine.js
@@ -1,16 +1,1 @@
-(function() {
-  var memoryInitializer = 'xash.html.mem';
-  if (typeof Module['locateFile'] === 'function') {
-    memoryInitializer = Module['locateFile'](memoryInitializer);
-  } else if (Module['memoryInitializerPrefixURL']) {
-    memoryInitializer = Module['memoryInitializerPrefixURL'] + memoryInitializer;
-  }
-  var xhr = Module['memoryInitializerRequest'] = new XMLHttpRequest();
-  xhr.open('GET', memoryInitializer, true);
-  xhr.responseType = 'arraybuffer';
-  xhr.send(null);
-})();
 
-var script = document.createElement('script');
-script.src = "xash.js";
-document.body.appendChild(script);
